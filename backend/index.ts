8f522ac574688cd6929cfb2cbed6779daa4b8de5
John-v77 <john.vs11@hotmail.com>
2026-05-18 18:25:39 -0400

    backend - server.ts

--- backend/index.ts
+++ backend/index.ts
@@ -1,17 +1,7 @@
-import express, { Request, Response } from 'express';
-import dotenv from 'dotenv';
+import { createServer } from "./server";
 
-dotenv.config();
+const server = createServer();
 
-const app = express();
-const port = process.env.PORT || 3000;
-
-app.use(express.json());
-
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello world');
-});
-
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
+server.listen(3000, () => {
+  console.log("Server is running on http://localhost:3000");
 });
