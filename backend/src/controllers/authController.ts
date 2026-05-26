73318cd140cf04e1623a7054e8cbb3121c93cacc
John-v77 <john.vs11@hotmail.com>
2026-05-26 15:28:24 -0400

    adds userSignup controller

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -1,6 +1,24 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
+import catchAsync from "../utils/catchAsync";
+import { User, UserModel } from "../models/userModel";
 
-export const signup = (req: Request, res: Response): void => {
-  console.log(req.body);
-  res.status(201).send("User signup");
-};
+export const signup = catchAsync(
+  async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    const { name, email, password, passwordConfirm, photo } = req.body;
+
+    const newUser: User = await UserModel.create({
+      name,
+      email,
+      password,
+      passwordConfirm,
+      photo,
+    });
+
+    res.status(201).json({
+      status: "success",
+      data: {
+        user: newUser,
+      },
+    });
+  },
+);
