e4fa5eb5001fad3c4f42a20a56268c9d16a9b834
John-v77 <john.vs11@hotmail.com>
2026-08-10 14:54:20 -0400

    adds Navbar component

---
+++ frontend/src/components/Navbar/navbar.component.tsx
@@ -0,0 +1,34 @@
+import React from "react"
+import "./navbar.css"
+import { Link, Outlet } from "react-router-dom"
+
+function Navbar() {
+  return (
+    <>
+      <nav className="navigation">
+        <div className="nav-container">
+          <Link to="/" className="nav-logo">
+            Natours
+          </Link>
+          <div className="nav-links">
+            <Link to="/tours" className="nav-link">
+              Tours
+            </Link>
+            <Link to="/about" className="nav-link">
+              About
+            </Link>
+            <Link to="/contact" className="nav-link">
+              Contact
+            </Link>
+            <Link to="/auth" className="nav-link nav-link-cta">
+              Sign In
+            </Link>
+          </div>
+        </div>
+      </nav>
+      <Outlet />
+    </>
+  )
+}
+
+export default Navbar
